1830dc13ba1b7950c1a05cd7625db8a3b3a40f1e
Alexey Zharov <to.alex.zharov@gmail.com>
2019-06-27 19:55:57 +0300

    Add bootstrap.css, made some refactoring (#18)
    * Add bootstrap.css, made some refactoring
    
    * Remove extra parameter

---
+++ src/js/popup/view/view.js
@@ -0,0 +1,51 @@
+import { isTimerActive } from '../actions/timer';
+
+import imgStart from '../../../img/start-gray-16.png';
+import imgStop from '../../../img/stop-gray-16.png';
+
+const timerBtn = document.getElementById("timer");
+
+function updateView(data) {
+  updateButtonView(isTimerActive(data));
+  updateInputsView(data.timer.projectId, data.timer.mergeRequestId);
+}
+
+function updateInputsView(projectId, mergeRequestId) {
+  if (projectId == undefined || mergeRequestId == undefined) {
+    return;
+  }
+  setProjectId(projectId);
+  setMergeRequestId(mergeRequestId);
+}
+
+function updateButtonView(started) {
+  const timerBtn = document.getElementById("timer");
+
+  if (started) {
+    timerBtn.classList.add("started");
+    browser.browserAction.setIcon({path: imgStop});
+  }
+  else {
+    timerBtn.classList.remove("started");
+    browser.browserAction.setIcon({path: imgStart});
+  }
+}
+
+function getProjectId() {
+  return document.getElementById("project-id").value;
+}
+
+function setProjectId(value) {
+  document.getElementById("project-id").value = value;
+}
+
+function getMergeRequestId() {
+  return document.getElementById("merge-request-id").value;
+}
+
+function setMergeRequestId(value) {
+  document.getElementById("merge-request-id").value = value;
+}
+
+// TODO: don't export timerBtn.
+export {updateView, updateInputsView, getProjectId, getMergeRequestId, addOnTimerClickEventHandler, timerBtn}
