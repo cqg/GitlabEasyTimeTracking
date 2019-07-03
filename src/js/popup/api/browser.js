fe07a51429348a18484d32bdcf228bf405752ce6
Dmitry Matrohin <matrokhin.d@gmail.com>
2019-07-03 12:44:31 +0300

    Promisify API calls
    It allows to chain them easily.
    Also show alert when logWorkTime fails to not lose spent time.

--- src/js/popup/api/browser.js
+++ src/js/popup/api/browser.js
@@ -1,16 +1,16 @@
-export function retrieveTimerData(onSuccess, onError) {
-  browser.storage.local.get(null).then(onSuccess, onError);
+export function retrieveTimerData() {
+  return browser.storage.local.get(null);
 }
 
-export function storeTimerData(data, onSuccess, onError) {
-  browser.storage.local.set(data).then(onSuccess, onError);
+export function storeTimerData(data) {
+  return browser.storage.local.set(data);
 }
 
-export function retrieveSettings(onSuccess, onError) {
-  browser.storage.sync.get(["token", "hostname"]).then(onSuccess, onError);
+export function retrieveSettings() {
+  return browser.storage.sync.get(["token", "hostname"]);
 }
 
-export function retrievePageUrl(onSuccess, onError) {
-  browser.tabs.query({active:true,currentWindow:true}).then((tabs) => onSuccess(tabs[0].url), onError);
+export function retrievePageUrl() {
+  return browser.tabs.query({active:true,currentWindow:true}).then((tabs) => tabs[0].url);
 }
 
--- src/js/popup/api/gitlab.js
+++ src/js/popup/api/gitlab.js
@@ -1,16 +1,36 @@
-function reqListener() {
-  console.log(this.responseText);
+function makeRequest(method, url, token) {
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+
+    request.open(method, url, true);
+    request.setRequestHeader("PRIVATE-TOKEN", token);
+
+    request.onloadend = () => {
+      if (request.status >= 200 && request.status < 300) {
+        resolve(request.response);
+      }
+      else {
+        reject({
+          status: request.status,
+          statusText: request.statusText
+        });
+      }
+    }
+
+    request.send();
+  });
+}
+
+function getApiRootUrl(hostname) {
+  return `${hostname}/api/v4`;
+}
+
+function getMergeRequestPath(projectId, mergeRequestId) {
+  return `/projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestId}`;
 }
 
 export function logWorkTime(hostname, token, projectId, mergeRequestId, spentTime) {
-  let request = new XMLHttpRequest();
-  let params = `duration=${spentTime}s`;
-  let path = `/api/v4/projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestId}/add_spent_time`;
-  console.log(hostname + path, params);
-
-  request.addEventListener("load", reqListener);
-  request.open("POST", `${hostname}${path}?${params}`, true);
-  request.setRequestHeader("PRIVATE-TOKEN", token);
-  request.send();
+  const url = getApiRootUrl(hostname) + getMergeRequestPath(projectId, mergeRequestId) + `/add_spent_time?duration=${spentTime}s`;
+  return makeRequest('POST', url, token);
 }
 
