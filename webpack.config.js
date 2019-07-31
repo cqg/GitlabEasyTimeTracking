c9a0664600be533d2b5ec105bec6921a161ea8cc
Dmitry Matrohin <matrokhin.d@gmail.com>
2019-07-31 21:03:35 +0300

    Fix build process
    Add build-prod for building production
    It sets needed values to manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var webpack = require("webpack"),
+const webpack = require("webpack"),
   path = require("path"),
   fileSystem = require("fs"),
   env = require("./utils/env"),
@@ -7,20 +7,13 @@ var webpack = require("webpack"),
   HtmlWebpackPlugin = require("html-webpack-plugin"),
   WriteFilePlugin = require("write-file-webpack-plugin");
 
-// load the secrets
-var alias = {};
+const fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
 
-var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
+const buildMode = process.env.NODE_ENV || "development";
 
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
-
-if (fileSystem.existsSync(secretsPath)) {
-  alias["secrets"] = secretsPath;
-}
-
-var options = {
+const options = {
   node: false,
-  mode: "production",
+  mode: buildMode,
   entry: {
     popup: path.join(__dirname, "src", "js", "popup.js"),
     options: path.join(__dirname, "src", "js", "options.js")
@@ -55,9 +48,6 @@ var options = {
       }
     ]
   },
-  resolve: {
-    alias: alias
-  },
   plugins: [
     // clean the build folder
     new CleanWebpackPlugin(["build"]),
@@ -67,9 +57,13 @@ var options = {
       from: "src/manifest.json",
       transform: function (content, path) {
         // generates the manifest file using the package.json informations
+        const securityPolicy = buildMode == 'production' ?
+          "script-src 'self'; object-src 'self'" :
+          "script-src 'self' 'unsafe-eval'; object-src 'self'";
         return Buffer.from(JSON.stringify({
           description: process.env.npm_package_description,
           version: process.env.npm_package_version,
+          content_security_policy: securityPolicy,
           ...JSON.parse(content.toString())
         }))
       }
